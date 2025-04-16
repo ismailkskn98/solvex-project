fe5b64df4c7b79ac9ee8bddf0f5d265df7a068f5
İsmail Keskin <hamdismailkskn98@gmail.com>
2025-04-16 09:54:55 +0300

    stil: ServiceCard bileşeninin yapısı ve stilleri güncellendi

--- src/components/home/services/serviceCard.jsx
+++ src/components/home/services/serviceCard.jsx
@@ -4,17 +4,17 @@ import { GridBackground } from "@/components/ui/gridBackground";
 import React from "react";
 
 export default function ServiceCard({ title, content, icon }) {
+  const Icon = icon;
   return (
-    <article className="group">
-      <GlareCard className="relative flex w-full flex-col items-start justify-end bg-transparent text-white">
-        <GridBackground />
-        <div className="absolute inset-0 -z-10 h-full w-full bg-gradient-to-t from-black to-transparent"></div>
-        <div className="flex h-[310px] w-full flex-col items-start gap-2 bg-gradient-to-t from-black to-transparent px-8 py-10">
-          <HeroIcon icon={icon} />
-          <h4 className="font-medium text-white">{title}</h4>
-          <p className="text-white/70">{content}</p>
-        </div>
-      </GlareCard>
-    </article>
+    <GlareCard className="group relative z-0 flex w-full flex-col items-start justify-end bg-transparent text-white">
+      <GridBackground className="pointer-events-none -z-20" />
+      <Icon className="pointer-events-none absolute -top-16 left-1/2 -z-10 -translate-x-1/2 text-[350px] text-white/10 opacity-30" />
+      <div className="pointer-events-none absolute inset-0 -z-[9] h-full w-full bg-gradient-to-t from-black to-transparent"></div>
+      <div className="pointer-events-none -z-[5] flex h-[275px] w-full flex-col items-start gap-2 bg-gradient-to-t from-black to-transparent px-8 pt-10 pb-3">
+        <HeroIcon icon={icon} />
+        <h4 className="font-medium text-white">{title}</h4>
+        <p className="text-sm text-white/70">{content}</p>
+      </div>
+    </GlareCard>
   );
 }
